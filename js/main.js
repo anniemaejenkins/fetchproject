327968ab6e6680688a391228facf5986aa66d1db
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-09 16:30:08 -0400

    added more stuff

--- js/main.js
+++ js/main.js
@@ -11,15 +11,17 @@ var container = document.getElementById('pageDiv');
 var profileRow = document.getElementById('sideDiv');
 var repoRow = document.getElementById('mainDiv');
 var bioRow = document.getElementById('bioDiv');
+var topRow = document.getElementById('topDiv');
+var profilePic = document.getElementById('profilePic');
 
   if(GITHUB_TOKEN){
 //set the AJAX header to send the token
 
   headers['Authorization'] = 'token ' + GITHUB_TOKEN;
   }
+
 //the second headers is the variable that is above the if statement
 //it's common to name the variable the same name as the property of the object you're requesting
-
 fetch(url, {headers: headers}).then(function(response){
 response.json().then(function(data){
   var user = data;
@@ -38,6 +40,11 @@ fetch(repoUrl).then(function(response){
       var repoClick = document.createElement('a');
       repoClick.innerHTML = '<a href="' + repoObject.html_url + '">' + repoObject.name + '</a>';
       repoDiv.appendChild(repoClick);
+
+      var repoTime = document.createElement('p');
+      repoTime.innerHTML = repoObject.updated_at;
+      repoDiv.appendChild(repoTime);
+      console.log(repoObject);
     }
   });
 });
@@ -46,17 +53,26 @@ fetch(repoUrl).then(function(response){
 
 
 function displayPage(user){
-  var profilePic = document.createElement('div');
-  profilePic.innerHTML = '<img src="' + user.avatar_url + '">';
-  profileRow.appendChild(profilePic);
+
+  // var profilePic = document.createElement('div');
+  profilePic.src = user.avatar_url;
+  // profileRow.appendChild(profilePic);
 
   var profileName = document.createElement('h1');
   profileName.innerHTML = user.name;
   bioRow.appendChild(profileName);
 
-  var userName = document.createElement('h2');
+  var userName = document.createElement('h3');
   userName.innerHTML = user.login;
   bioRow.appendChild(userName);
+
+  var userBio = document.createElement('p');
+  userBio.innerHTML = user.bio;
+  bioRow.appendChild(userBio);
+
+  
+
+
 
 }
 
